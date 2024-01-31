9ff5b3a74cfecc481ed83c6e93252c51be6c8a38
Omar <omarfkuri@gmail.com>
2024-01-31 16:26:55 -0600

    Local Resolve remove keep

--- src/api/localResolve.ts
+++ src/api/localResolve.ts
@@ -40,18 +40,6 @@ export const localResolve: BundlerConfig["onParse"] = (bab, _, filename) => {
 
   bab.traverse({
     ImportDeclaration(path) {
-      if (path.node.leadingComments) {
-        const comment = path.node.leadingComments[path.node.leadingComments.length-1];
-        if (comment && comment.value.match(/\s*@keep\s*/)) {
-          if (path.node.leadingComments?.length) {
-            path.node.leadingComments = path.node.leadingComments.filter(e => e && !e.value.match(/\s*@keep\s*/))
-          }
-          if (path.node.trailingComments?.length) {
-            path.node.trailingComments = path.node.trailingComments.filter(e => e && !e.value.match(/\s*@keep\s*/))
-          }
-          return;
-        }
-      }
       if (path.node.source.type == "StringLiteral") {
         path.node.source.value = makeFile(pastDir, path.node.source.value);
       }
